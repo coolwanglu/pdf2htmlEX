f1f4155371736525cde64d7e5b23ed6cbe70a257
Lu Wang <coolwanglu@gmail.com>
2012-08-16 00:22:27 +0800

    title extraction

--- src/PaperClub.h
+++ src/PaperClub.h
@@ -8,17 +8,25 @@
 #ifndef PAPERCLUB_H__
 #define PAPERCLUB_H__
 
+#include <sstream>
+
 #include "HTMLRenderer.h"
 #include "HTMLRenderer/namespace.h"
+
+using std::ostringstream;
 
 class PC_HTMLRenderer : public HTMLRenderer
 {
     public:
         PC_HTMLRenderer (const Param * param)
            : HTMLRenderer(param)
+           , max_font_size (0)
         { } 
 
-        virtual ~PC_HTMLRenderer() { }
+        virtual ~PC_HTMLRenderer() 
+        { 
+            cerr << "Title: " << cur_title.str() << endl;
+        }
 
         virtual void pre_process() 
         {
@@ -39,6 +47,56 @@ class PC_HTMLRenderer : public HTMLRenderer
             HTMLRenderer::endPage();
             html_fout.close();
         }
+
+        virtual void drawString(GfxState * state, GooString * s)
+        {
+            //determine padding now
+            string padding =  (_equal(cur_ty, ty) && (cur_tx < tx + EPS)) ? "" : " ";
+            
+            // wait for status update
+            HTMLRenderer::drawString(state, s);
+
+            if(pageNum != 1) return;
+
+            if((state->getRender() & 3) == 3) return;
+            
+            auto font = state->getFont();
+            if(font == nullptr) return;
+
+            if(cur_font_size < max_font_size) return;
+            if(cur_font_size > max_font_size)
+            {
+                max_font_size = cur_font_size;
+                cur_title.str("");
+            }
+            else
+            {
+                cur_title << padding;
+            }
+
+            char *p = s->getCString();
+            int len = s->getLength();
+            
+            CharCode code;
+            Unicode * u;
+            int uLen;
+            double dx, dy, ox, oy;
+
+            while(len > 0)
+            {
+                int n = font->getNextChar(p, len, &code, &u, &uLen, &dx, &dy, &ox, &oy);
+                outputUnicodes(cur_title, u, uLen);
+                p += n;
+                len -= n;
+            } 
+
+            tx = cur_tx;
+            ty = cur_ty;
+        }
+
+        double max_font_size;
+        double tx, ty;
+        ostringstream cur_title;
 };
 
 
