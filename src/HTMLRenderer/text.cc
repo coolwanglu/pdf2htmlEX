3b44d3059ce539fb728048b3411906ace16012d8
Lu Wang <coolwanglu@gmail.com>
2012-08-16 12:59:31 +0800

    shit left within a line

--- src/HTMLRenderer/text.cc
+++ src/HTMLRenderer/text.cc
@@ -172,8 +172,9 @@ void HTMLRenderer::drawString(GfxState * state, GooString * s)
         }
         else
         {
-            // or can we shift left using simple tags?
-            close_cur_line();
+            // shift left
+            // TODO, create a class for this
+            html_fout << format("<span style=\"margin-left:%1%px\"></span>") % target;
         }
     }
 
