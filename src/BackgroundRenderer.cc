b7dcad3632fc10694c5da54e4aa470aa1c055613
Lu Wang <coolwanglu@gmail.com>
2012-09-19 03:30:20 +0800

    ..

--- src/BackgroundRenderer.cc
+++ src/BackgroundRenderer.cc
@@ -18,6 +18,6 @@ void BackgroundRenderer::drawChar(GfxState *state, double x, double y,
   double originX, double originY,
   CharCode code, int nBytes, Unicode *u, int uLen)
 {
-    SplashOutputDev::drawChar(state,x,y,dx,dy,originX,originY,code, nBytes, u, uLen);
+//SplashOutputDev::drawChar(state,x,y,dx,dy,originX,originY,code, nBytes, u, uLen);
 }
 
