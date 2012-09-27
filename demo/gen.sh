4a5ec42edeaf1e81c2133305e1a9337f7ba553e3
Lu Wang <coolwanglu@gmail.com>
2012-09-27 12:56:41 +0800

    added new demo pages

--- demo/gen.sh
+++ demo/gen.sh
@@ -1,1 +1,9 @@
-for f in ../test/demo/*.pdf; do pdf2htmlEX --external-hint-tool=ttfautohint --auto-hint=1 --zoom 2 $f; done
+#!/bin/bash
+
+PARA="--external-hint-tool=ttfautohint --auto-hint=1 --fit-width 1024"
+
+for f in ../test/demo/*.pdf; do 
+    pdf2htmlEX $PARA $f
+done
+
+pdf2htmlEX $PARA -l 3 ../test/demo/issue65_en.pdf issue65_en_sample.html
