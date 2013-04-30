e65791e5ae424c213fec83ae5bec2beb70767ebd
Lu Wang <coolwanglu@gmail.com>
2013-04-30 15:58:26 +0800

    embed-exteranl-font is on by default

--- src/pdf2htmlEX.cc
+++ src/pdf2htmlEX.cc
@@ -81,7 +81,7 @@ void parse_options (int argc, char **argv)
         
         // fonts
         .add("embed-base-font", &param.embed_base_font, 1, "embed local match for standard 14 fonts")
-        .add("embed-external-font", &param.embed_external_font, 0, "embed local match for external fonts")
+        .add("embed-external-font", &param.embed_external_font, 1, "embed local match for external fonts")
         .add("font-suffix", &param.font_suffix, ".ttf", "suffix for embedded font files (.ttf,.otf,.woff,.svg)")
         .add("decompose-ligature", &param.decompose_ligature, 0, "decompose ligatures, such as \uFB01 -> fi")
         .add("remove-unused-glyph", &param.remove_unused_glyph, 1, "remove unused glyphs in embedded fonts")
