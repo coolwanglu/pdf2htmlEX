84988c7c2863383306a530223d4f09438a71c96c
Lu Wang <coolwanglu@gmail.com>
2013-04-30 12:20:31 +0800

    support multiple distribution for PPA

--- build_for_ppa.py
+++ build_for_ppa.py
@@ -15,6 +15,10 @@ import sys
 import re
 import time
 
+package='pdf2htmlex'
+SUPPORTED_DIST=('precise', 'quantal', 'raring')
+dist_pattern=re.compile('|'.join(['\\) '+i for i in SUPPORTED_DIST]))
+
 print 'Generating version...'
 
 try:
@@ -24,8 +28,7 @@ except:
     sys.exit(-1)
 
 today_timestr = time.strftime('%Y%m%d%H%M')
-package='pdf2htmlex'
-projectname='pdf2htmlEX'
+projectdir=os.getcwd()
 try:
     version = re.findall(r'set\(PDF2HTMLEX_VERSION\s*"([^"]*)"\)', open('CMakeLists.txt').read())[0]
 except:
@@ -36,18 +39,27 @@ deb_version = version+'-1~git'+today_timestr+'r'+rev
 full_deb_version = deb_version+'-0ubuntu1'
 
 #check if we need to update debian/changelog
-if re.findall(r'\(([^)]+)\)', open('debian/changelog').readline())[0] == full_deb_version:
-    print
-    print 'No need to update debian/changelog, skipping'
-else:
-    print
-    print 'Writing debian/changelog'
-    if os.system('dch -v "%s"' % (full_deb_version,)) != 0:
-        print 'Failed when updating debian/changelog'
+with open('debian/changelog') as f:
+    if re.findall(r'\(([^)]+)\)', f.readline())[0] == full_deb_version:
+        print
+        print 'No need to update debian/changelog, skipping'
+    else:
+        print
+        print 'Writing debian/changelog'
+        if os.system('dch -v "%s"' % (full_deb_version,)) != 0:
+            print 'Failed when updating debian/changelog'
+            sys.exit(-1)
+
+    f.seek(0)
+    #check dist mark of changelog
+    changelog = f.read()
+    m = dist_pattern.search(changelog)
+    if m is None or m.pos >= changelog.find('\n'):
+        print 'Cannot locate the dist name in the first line of changelog'
         sys.exit(-1)
 
 print
-print 'Building...'
+print 'Preparing build ...'
 # handling files
 if os.system('(rm CMakeCache.txt || true) && cmake . && make dist') != 0:
     print 'Failed in creating tarball'
@@ -79,28 +91,38 @@ except:
     print 'Cannot enter project dir'
     sys.exit(-1)
 
-os.system('cp -r ../../%s/debian .' % (projectname,))
+os.system('cp -r %s/debian .' % (projectdir,))
 
-# building
-if os.system('debuild -S -sa') != 0:
-    print 'Failed in debuild'
-    sys.exit(-1)
+for cur_dist in SUPPORTED_DIST:
+    print
+    print 'Building for ' + cur_dist + ' ...'
+    # substitute distribution name 
+    with open('debian/changelog', 'w') as f:
+        f.write(dist_pattern.sub('~%s1) %s' % (cur_dist, cur_dist), changelog, 1))
 
-print
-sys.stdout.write('Everything seems to be good so far, upload?(y/n)')
-sys.stdout.flush()
-ans = raw_input().lower()
-while ans not in ['y', 'n']:
-    sys.stdout.write('I don\'t understand, enter \'y\' or \'n\':')
+    # building
+    if os.system('debuild -S -sa') != 0:
+        print 'Failed in debuild'
+        sys.exit(-1)
+
+    """
+    print
+    sys.stdout.write('Everything seems to be good so far, upload?(y/n)')
+    sys.stdout.flush()
     ans = raw_input().lower()
+    while ans not in ['y', 'n']:
+        sys.stdout.write('I don\'t understand, enter \'y\' or \'n\':')
+        ans = raw_input().lower()
 
-if ans == 'n':
-    print 'Skipped.'
-    sys.exit(0)
-   
-if os.system('dput ppa:coolwanglu/%s ../%s' % (package, package+'_'+full_deb_version+'_source.changes')) != 0:
-    print 'Failed in uploading by dput'
-    sys.exit(-1)
+    if ans == 'n':
+        print 'Skipped.'
+        sys.exit(0)
+    """
+
+    print 'Uploading'   
+    if os.system('dput ppa:coolwanglu/%s ../%s' % (package, package+'_'+full_deb_version+'~'+cur_dist+'1_source.changes')) != 0:
+        print 'Failed in uploading by dput'
+        sys.exit(-1)
 
 print 'Build area not cleaned.'
 print 'All done. Cool!'
