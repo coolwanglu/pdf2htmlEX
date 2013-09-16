f45e4e1c36f1caaa8e39f3ecd0cb0996ed0dbc2b
Lu Wang <coolwanglu@gmail.com>
2013-09-16 20:05:17 +0800

    fix build

---
+++ src/BackgroundRenderer/BackgroundRenderer_forward.h
@@ -0,0 +1,29 @@
+/*
+ * Background renderer
+ * Render all those things not supported as Image
+ *
+ * Copyright (C) 2013 Lu Wang <coolwanglu@gmail.com>
+ */
+
+
+/*
+ * This file includes forward declarations since HTMLRenderer and BackgroundRendere have cross references
+ */
+
+#ifndef BACKGROUND_RENDERER_FORWARD_H__
+#define BACKGROUND_RENDERER_FORWARD_H__
+
+#include "pdf2htmlEX-config.h"
+
+namespace pdf2htmlEX {
+#if HAVE_CAIRO
+    class CairoBackgroundRenderer;
+    typedef CairoBackgroundRenderer BackgroundRenderer;
+#else
+    class SplashBackgroundRenderer;
+    typedef SplashBackgroundRenderer BackgroundRenderer;
+#endif // HAVE_CAIRO
+}
+
+
+#endif //BACKGROUND_RENDERER_FORWARD_H__
