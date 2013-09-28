2d8df9d647c5382b129e983813e5f91bf108f4b8
Lu Wang <coolwanglu@gmail.com>
2013-09-28 13:30:57 +0800

    update demo

--- demo/gen.sh
+++ demo/gen.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-PARA="--optimize-text 0 --split-pages 1 --external-hint-tool=ttfautohint --auto-hint=1 --fit-width 1024"
+PARA="--optimize-text 0 --split-pages 1 --external-hint-tool=ttfautohint --fit-width 1024"
 
 for f in ../test/demo/*.pdf; do 
     pdf2htmlEX $PARA $f
