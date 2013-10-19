a210576e46a437453ec948d7d369e61af0511ef6
Lu Wang <coolwanglu@gmail.com>
2013-10-19 15:06:32 +0800

    compress js

---
+++ share/js_src/compile.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+# Compile and optimize JS code
+# Copyright 2013 Lu Wang <coolwanglu@gmail.com>
+
+# To enable closure-compiler, you need to install and configure JAVA environment
+# Read 3rdparty/closure-compiler/README for details
+
+
+BASEDIR=$(dirname $0)
+OUTPUT="$BASEDIR/../pdf2htmlEX.js"
+
+(echo "Compiling pdf2htmlEX.js with closure-compiler..." && \
+    tmpfile=$(mktemp 2>/dev/null) && \
+    java -jar "$BASEDIR/../../3rdparty/closure-compiler/compiler.jar" --compilation_level ADVANCED_OPTIMIZATIONS --process_jquery_primitives --js "$BASEDIR/jquery.js" --js "$BASEDIR/css_class_names.js" --js "$BASEDIR/viewer.js" > "$tmpfile" 2>/dev/null && \
+    cat "$BASEDIR/header.js" "$tmpfile" > "$OUTPUT" && \
+    rm -f -- "$tmpfile") || \
+(echo "Failed. Fall back to naive concatenation" && \
+cat "$BASEDIR/header.js" "$BASEDIR/jquery.js" "$BASEDIR/css_class_names.js" "$BASEDIR/viewer.js" > "$OUTPUT")
+
+
---
+++ share/js_src/header.js
@@ -0,0 +1,3 @@
+/* pdf2htmlEX.js * Copyright 2012,2013 Lu Wang <coolwanglu@gmail.com> and other contributors * https://github.com/coolwanglu/pdf2htmlex * https://github.com/coolwanglu/pdf2htmlEX/blob/master/share/LICENSE */
+/*! Depends on jQuery v@1.8.1 jquery.com | jquery.org/license */
+window.pdf2htmlEX = window.pdf2htmlEX || {};
