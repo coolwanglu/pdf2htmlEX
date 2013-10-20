ec130dba7092b56a2f4e2cb3d57f2c5df6529e94
Lu Wang <coolwanglu@gmail.com>
2013-10-20 17:06:40 +0800

    improve compile.sh

--- share/js_src/compile.sh
+++ share/js_src/compile.sh
@@ -10,10 +10,7 @@ BASEDIR=$(dirname $0)
 OUTPUT="$BASEDIR/../pdf2htmlEX.js"
 
 (echo 'Building pdf2htmlEX.js with closure-compiler...' && \
-    tmpfile=$(mktemp 2>/dev/null) && \
-    java -jar "$BASEDIR/../../3rdparty/closure-compiler/compiler.jar" --compilation_level ADVANCED_OPTIMIZATIONS --process_jquery_primitives --externs "$BASEDIR/../jquery.js" --js "$BASEDIR/css_class_names.js" --js "$BASEDIR/viewer.js" > "$tmpfile" 2>/dev/null && \
-    cat "$BASEDIR/header.js" "$tmpfile" > "$OUTPUT" && \
-    rm -f -- "$tmpfile" && \
+    java -jar "$BASEDIR/../../3rdparty/closure-compiler/compiler.jar" --compilation_level ADVANCED_OPTIMIZATIONS --process_jquery_primitives --externs "$BASEDIR/../jquery.js" --js "$BASEDIR/header.js" --js "$BASEDIR/css_class_names.js" --js "$BASEDIR/viewer.js" > "$OUTPUT" 2>/dev/null && \
     echo 'Done.') || \
 (echo 'Failed. Read `3rdparty/closure-compiler/README` for more detail.' && \
 echo 'Fall back to naive concatenation' && \
