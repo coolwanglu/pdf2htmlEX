c2187ec68572c21e4aaf6ef5994b5dcfaa182732
Lu Wang <coolwanglu@gmail.com>
2013-10-20 15:31:59 +0800

    optimize pdf2htmlEX logo for web

--- logo/update_png.sh
+++ logo/update_png.sh
@@ -1,4 +1,8 @@
 #!/bin/sh
 
+# convert raw SVG into png of different sizes
 convert -background none -resize 64x64^ pdf2htmlEX.svg pdf2htmlEX-64x64.png
 convert -background none -resize 256x256^ pdf2htmlEX.svg pdf2htmlEX-256x256.png
+# optimize for web
+pngnq pdf2htmlEX-64x64.png
+mv pdf2htmlEX-64x64-nq8.png ../share/pdf2htmlEX-64x64.png
