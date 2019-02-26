ce83fd83a5574cc426ee704edd2d019161ff57dd
David Morton <david@isamorton.com>
2019-02-26 13:35:10 +0000

    Allowing more than one form field to have the same value

--- src/HTMLRenderer/form.cc
+++ src/HTMLRenderer/form.cc
@@ -25,6 +25,8 @@ void HTMLRenderer::process_form(ofstream & out)
     FormPageWidgets * widgets = cur_catalog->getPage(pageNum)->getFormWidgets();
     int num = widgets->getNumWidgets();
 
+    cerr << "number of widgets: " << num << endl;
+
     for(int i = 0; i < num; i++)
     {
         FormWidget * w = widgets->getWidget(i);
@@ -40,19 +42,33 @@ void HTMLRenderer::process_form(ofstream & out)
         double height = y2 - y1;
         double font_size = height / 2;           
            
+		tm_transform(default_ctm, x1, y1);
         
         if(w->getType() == formText)
         {
-
+	    cerr << "Found a text box" << endl;
             FormField *f = w->getField();
             Object *o = f->getObj();
 
             // the following line throws if the result of lookup doesnt return a string. 
             Object *lo = o->getDict()->lookup((char *)"T", o);
 
+            if (lo->getType() == objNull) {
+                // check the parent
+                Object *wObj = w->getObj();
+                cerr << "lo is obj null, so looking for parent" << endl;
+                Dict *wDict = wObj->getDict();
+                cerr << "Fetched parent dictionary" << endl;
+                Object *parent = wDict->lookup((char *)"Parent", wObj);
+                if (parent->getType() == objDict) {
+		   cerr << "Fetching from parent dictionary" << endl;
+                   lo = parent->getDict()->lookup((char *)"T", parent);
+                }
+            }
+            
             if (lo->getType() == objString) { 
                 char *name = lo->getString()->getCString();
-
+                cerr << "Writing text box for " << name << endl;
                 out << "<div id=\"text-" << pageNum << "-" << i
                     << "\" form-field=\"" << name
                     << "\" class=\"" << CSS::INPUT_TEXT_CN 
@@ -61,6 +77,10 @@ void HTMLRenderer::process_form(ofstream & out)
                     << " width: " << width << "px; height: " << std::to_string(height) 
                     << "px; line-height: " << std::to_string(height) << "px; font-size: " 
                     << font_size << "px;\" ></div>" << endl;
+            } else if (lo->getType() == objNull) {
+		//Object *parentText = o->getDict()->lookup((char *)"Parent", o);
+
+		cerr << "Found something else..." << o->getType() << endl; 
             }
         } 
         else if (w->getType() == formButton)
