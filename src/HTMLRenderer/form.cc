7bf60118ad0f7e535ef5066274cda2a92e44d0ff
Hunter Windham <hunter.windham@spottracker.com>
2023-06-02 15:22:44 -0500

    Changes to fix fontsize and alignmnet issues

--- src/HTMLRenderer/form.cc
+++ src/HTMLRenderer/form.cc
@@ -10,6 +10,7 @@
 #include <iostream>
 #include <sstream>
 #include <string>
+#include <algorithm>
 
 #include "HTMLRenderer.h"
 #include "util/namespace.h"
@@ -41,7 +42,8 @@ void HTMLRenderer::process_form(ofstream & out)
         double width = x2 - x1;
         double height = y2 - y1;
         double font_size = height / 2;           
-           
+        // Define font_unit with a default of px this will change to pt if there is a defined font in the pdf template
+        std::string font_unit = "px";
 		tm_transform(default_ctm, x1, y1);
         
         if(w->getType() == formText)
@@ -55,6 +57,18 @@ void HTMLRenderer::process_form(ofstream & out)
             o->getDict()->lookup((char *)"T", lo);
             Object *ff = new Object();
             o->getDict()->lookup((char *)"Ff", ff);
+
+            // DA is an null || string that represents a few things such font family, font size ect.
+            // Ex. looks like '/Helv 6 Tf 0 g' where Helv is the font family
+            // and 6 is the font size.
+            Object *da = new Object();
+            o->getDict()->lookup((char *)"DA", da);
+
+            // Q is an null || int that represents the text alignmnet if it is null or 0 it means
+            // left aligned which is default. 1 stands for center aligned and 2 stands for right aligned.
+            Object *q = new Object();
+            o->getDict()->lookup((char *)"Q", q);
+
             //Object *po = o->getDict()->lookup((char *)"T", o);
             cerr << "Success" << endl;
             //o = f->getObj(); 
@@ -83,7 +97,53 @@ void HTMLRenderer::process_form(ofstream & out)
                 Object *parent = wDict->lookup((char *)"Parent", wObj);
                 if (parent->getType() == objDict) {
 		   cerr << "Fetching from parent dictionary" << endl;
+
+                    // If lo was null this means that there is a parent with the
+                    // missing info. The same is likely the case both da and q which
+                    // live at the same level so we repeat the lookups if they are null.
+                    if (da->getType() == objNull) {
+                        parent->getDict()->lookup((char *)"DA", da);
+                    }
+
+                    if (q->getType() == objNull) {
+                        parent->getDict()->lookup((char *)"Q", q);
+                    }
+
                    lo = parent->getDict()->lookup((char *)"T", parent);
+                }
+            }
+
+            // If we have a value for da we want to use it to set the font size
+            // however, the font size is stored with other data so we must strip
+            // it down to just font size. Ex. looks like '/Helv 6 Tf 0 g'
+            // where Helv is the font family and 6 is the font size.
+            if (da->getType() == objString) {
+                // da is just a pointer so we need to output the value into a temporary variable.
+                std::string davalue = da->getString()->getCString();
+                // Removes everything from string that is not a digit or space
+                // this will convert it from looking like '/Helv 6 Tf 0 g'
+                // to looking something like '6 0'
+                davalue.erase(
+                    std::remove_if(davalue.begin(), davalue.end(), [](char ch) { return !std::isdigit(ch) && !std::isspace(ch);}),
+                    davalue.end()
+                );
+
+                // Call stoi() passing the davalue this will grab the first number and stop at space character
+                // this will convert it from something like '6 0' to '6'.
+                font_size = std::stoi(davalue);
+                // If da has a value then we will have a font size that needs to be pt not px
+                font_unit = "pt";
+            }
+
+            // Default value for text_align is left
+            std::string text_align = "left";
+            // If we have a q value this means that we have an alignmnet and must set it 
+            // based off of 1 for center and 2 for right alignment.
+            if (q->getType() == objInt) { 
+                if(q->getInt() == 1) {
+                    text_align = "center";
+                } else if(q->getInt() == 2) {
+                    text_align = "right";
                 }
             }
             
@@ -97,9 +157,10 @@ void HTMLRenderer::process_form(ofstream & out)
                     << "\" class=\"" << classes 
                     << "\" style=\"position: absolute; font-family:arial; left: " << x1 
                     << "px; bottom: " << y1 << "px;" 
+                    << " text-align: " << text_align << ";"
                     << " width: " << width << "px; height: " << std::to_string(height) 
                     << "px; line-height: " << std::to_string(height) << "px; font-size: " 
-                    << font_size << "px;\" ></div>" << endl;
+                    << font_size << font_unit << ";\" ></div>" << endl;
             } else if (lo->getType() == objNull) {
 		//Object *parentText = o->getDict()->lookup((char *)"Parent", o);
 
